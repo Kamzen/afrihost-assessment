c75d71dbdab52b5bc9b55448bfd5f387132addc6
Kamzen <kamzen1994@gmail.com>
2022-04-03 22:30:49 +0200

    Some Progress

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TopNavigation />
+  <SlideShow />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TopNavigation from '@/components/topNavigation';
+import SlideShow from "@/components/slideShow";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TopNavigation,
+    SlideShow
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+  *{
+    box-sizing: border-box;
+  }
+  .bd{
+    border : 0.05em solid rgba(211,211,211,.275);
+  }
+  @font-face {
+    font-family: Medium;
+    src: url("./assets/fonts/Montserrat/Montserrat-Medium.ttf");
+  }
+  @font-face {
+    font-family: SemiBold;
+    src: url("./assets/fonts/Montserrat/Montserrat-SemiBold.ttf");
+  }
+
 </style>
---
+++ src/components/topNavigation.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="top-nav">
+    <div class="bd top-nav-left-icon">
+      <img class="bd" :src="slideList" alt="'" />
+    </div>
+    <div class="bd top-nav-text">
+      <p>Explore</p>
+    </div>
+    <div class="bd top-nav-right-icon">
+      <img class="bd" :src="notificationIcon" alt="" />
+      <img class="bd" :src="searchIcon" alt="" />
+    </div>
+  </div>
+</template>
+
+
+<script>
+
+import slideList from '../assets/images/sidelist.svg';
+import notificationIcon from '../assets/images/notification-icon.svg';
+import searchIcon from '../assets/images/search-icon.svg';
+
+export default {
+  name: 'TopNavigation',
+  props: {
+    msg: String
+  },
+  setup(){
+    return{
+      slideList, notificationIcon, searchIcon
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.top-nav {
+  height: 60px;
+  width: 100%;
+  background-color: white;
+  padding: 0;
+  margin: 0mm;
+  display: grid;
+  grid-template-columns: 1fr 5fr 2fr;
+
+  .top-nav-text {
+    font-family: Medium;
+    text-align: center;
+    color: #222222;
+  }
+  .top-nav-left-icon{
+    text-align: center;
+    padding-top: 15px;
+    img{
+      width: 27px;
+      height: 27px;
+    }
+  }
+  .top-nav-right-icon{
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    img{
+      width: 27px;
+      height: 27px;
+      margin: auto;
+    }
+  }
+
+}
+</style>
